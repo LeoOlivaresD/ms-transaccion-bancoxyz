57bdee069112965c3af3fabd5d43ad8d06c6a93d
LeoOlivaresD <cspraword@gmail.com>
2025-09-12 21:34:09 -0300

    commit inicial ms transaccion

---
+++ src/main/java/com/duoc/ms_transaccion_bancoxyz/MsTransaccionBancoxyzApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.ms_transaccion_bancoxyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTransaccionBancoxyzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTransaccionBancoxyzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duoc/ms_transaccion_bancoxyz/MsTransaccionBancoxyzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.ms_transaccion_bancoxyz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTransaccionBancoxyzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
